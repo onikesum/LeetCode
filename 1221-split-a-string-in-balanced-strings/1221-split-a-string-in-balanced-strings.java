4ae97cf2950c3d36908f3b5a2f312860834036e5
onikesum <122078008+onikesum@users.noreply.github.com>
2024-06-05 15:32:12 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (5.26%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,19 +1,25 @@
 import java.util.*;
 class Solution {
     public int balancedStringSplit(String s) {
-        int Rcount = 0;
-        int Lcount = 0;
-        int split = 0;
-        for(int i = 0; i<s.length(); i++){
-            if(s.charAt(i)=='R')
-                Rcount++;
-            else
-                Lcount++;
-            if(Rcount==Lcount){
+        // int Rcount = 0;
+        // int Lcount = 0;
+        // int split = 0;
+        // for(int i = 0; i<s.length(); i++){
+        //     if(s.charAt(i)=='R')
+        //         Rcount++;
+        //     else
+        //         Lcount++;
+        //     if(Rcount==Lcount){
+        //         split++;
+        //         Rcount = 0;
+        //         Lcount = 0;
+        //     }
+        // }
+        int count = 0; int split=0;
+        for(int i=0; i<s.length();i++){
+            count += s.charAt(i) == 'R' ? 1 : -1;
+            if(count==0)
                 split++;
-                Rcount = 0;
-                Lcount = 0;
-            }
         }
         return split;
     }
