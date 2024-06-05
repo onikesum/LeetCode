9ae39916bcfa4c14c94ccc966a479ef9093930af
onikesum <122078008+onikesum@users.noreply.github.com>
2024-06-05 15:16:25 +0900

    Time: 1 ms (21.62%), Space: 41 MB (75.13%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int balancedStringSplit(String s) {
+        int Rcount = 0;
+        int Lcount = 0;
+        int split = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)=='R')
+                Rcount++;
+            else
+                Lcount++;
+            if(Rcount==Lcount){
+                split++;
+                Rcount = 0;
+                Lcount = 0;
+            }
+        }
+        return split;
+    }
+}
