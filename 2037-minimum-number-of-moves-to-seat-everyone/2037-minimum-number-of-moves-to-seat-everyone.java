118c7d235ff2fa8983e951806cb2ba55215474c3
onikesum <122078008+onikesum@users.noreply.github.com>
2024-06-13 21:31:28 +0900

    Time: 3 ms (62.42%), Space: 43.8 MB (90.27%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        int total = 0;
+        Arrays.sort(seats); Arrays.sort(students);
+        for(int i = 0; i < seats.length; i++){
+            total += Math.abs(students[i] - seats[i]);     
+        }
+        return total;
+    }
+}
